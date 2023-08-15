f73ba28c86fcefa7906ef48bcf3049fa8adee84e
Nii Ayitey Aryeetey <kidpato6@gmail.com>
2023-08-15 12:48:56 -0700

    Betty style coding

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int string[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, size;
-	
+
 	size = sizeof(string) / sizeof(int);
-	
+
 	for (count = 0; count < size; count++)
 	{
 		_putchar(string[count]);
